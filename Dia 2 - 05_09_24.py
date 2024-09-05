3e58cc63c35ea79df8b3c0e0cb6f543a2d0fcf73
naiaragama <santosnaia@gmail.com>
2024-09-04 23:05:13 -0300

    Dia 2 primeiro código do desafio

---
+++ Dia 2 - 05_09_24.py
@@ -0,0 +1,7 @@
+# Desafio: Escreva um Programa que recebe dois números do usuário e imprima a soma deles.
+
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+soma = numero1 + numero2
+
+print(f'A soma dos dois números {numero1} e {numero2} é igual: {soma}')
