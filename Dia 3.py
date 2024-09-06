1cc2217fce22be55a5d143bc004e7c4561130719
naiaragama <santosnaia@gmail.com>
2024-09-05 21:55:02 -0300

    Desafio Dia 3

--- Dia 3.py
+++ Dia 3.py
@@ -1,10 +1,10 @@
 # Desafio: Escreva um programa que verifica se um número é par ou ímpar.
 
-numero = int(input('Digite um número: '))
+numero = int(input("Digite um número: "))
 
 resultado = numero % 2
 
 if resultado == 0:
-    print('O numero digitado é par')
+    print("O numero digitado é par")
 else:
-    print('O número digitado é impar')
+    print("O número digitado é impar")
