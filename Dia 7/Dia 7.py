4426bad3e0a582dcff2804999e51dcc4fa1b1e10
naiaragama <santosnaia@gmail.com>
2024-09-10 22:04:27 -0300

    Ajustes no desafio dia  7

--- Dia 7/Dia 7.py
+++ Dia 7/Dia 7.py
@@ -1,6 +1,5 @@
 # Escreva um programa que calcula a tabuada de um número. (Python)
 
-# numero = int(input('Digite o número que deseja calcular a tabuada: '))
 numero = int(input('Digite o número que deseja calcular a tabuada, digite um numero de 1 a 10: '))
 
 print(f"\nTabuada do número {numero}:")
